a8f3e11c9593e8082b00a2d4c6040caf0cb050e5
Oguzhanolgun <oguzhanolgun35.5@gmail.com>
2021-06-15 19:17:15 +0300

    Added ingredients

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,10 +1,15 @@
 import React from "react";
-const Recipe = () => {
+const Recipe = ({ title, calories, img, ingredients }) => {
   return (
     <div>
-      <h1>Title</h1>
-      <p>Calories</p>
-      <img src="" alt="" />
+      <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => (
+          <li>{ingredient.text}</li>
+        ))}
+      </ol>
+      <p>{calories}</p>
+      <img src={img} alt="" />
     </div>
   );
 };
